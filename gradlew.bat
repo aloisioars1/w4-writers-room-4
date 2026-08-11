de7d4577dc761a3ec0ee95c9b092b06cb96b6e16
aloisioars1 <88538737+aloisioars1@users.noreply.github.com>
2026-08-11 01:21:15 -0300

    Add gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,6 @@
+@if "%DEBUG%" == "" @echo off
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+java -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
