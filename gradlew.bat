35882f1fc8a0509ca42953f01c1c36f2621dac33
aloisioars1 <88538737+aloisioars1@users.noreply.github.com>
2026-08-16 03:55:57 -0300

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,6 +0,0 @@
-@if "%DEBUG%" == "" @echo off
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-java -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
